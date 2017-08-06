ba9dc628efbaed0e00b16776f5ee1af2e36bbec5
Yagami <y034112@gmail.com>
2017-08-06 16:10:08 +0900

    Add files via upload

--- RandomForests_scikit-learn/main3.py
+++ RandomForests_scikit-learn/main3.py
@@ -21,7 +21,7 @@ def main():
     #======================================================================================
     # ランダムフォレスト [random forests] による識別問題（３クラス）
     # アヤメデータの３品種の分類　（Python & scikit-learn ライブラリを使用）   
-    # ランダムフォレストによるデータ解析（各特徴の誤り率、OBB error rate）
+    # ランダムフォレストによるデータ解析（特徴の重要さ）
     #======================================================================================
 
     #====================================================
@@ -34,9 +34,10 @@ def main():
 
     # scikit-learn ライブラリから iris データを読み込む
     iris = datasets.load_iris()
+    print(iris.data)
 
-    # 3,4 列目の特徴量を抽出し、dat_X に保管
-    dat_X = iris.data[ :, [2,3] ]
+    # 全てのの特徴量を抽出し, dat_X に保管
+    dat_X = iris.data
 
     # クラスラベル（教師データ）を取得
     dat_y = iris.target
@@ -76,129 +77,78 @@ def main():
     X_combined_std = numpy.vstack( (X_train_std, X_test_std) )  # list:(X_train_std, X_test_std) で指定
     y_combined     = numpy.hstack( (y_train, y_test) )
 
-    # 学習データを正規化（後で plot データ等で使用する）
-    dat_X_std = numpy.copy(dat_X)                                           # ディープコピー（参照コピーではない）
-    dat_X_std[:,0] = ( dat_X[:,0] - dat_X[:,0].mean() ) / dat_X[:,0].std()  # 0列目全てにアクセス[:,0]
-    dat_X_std[:,1] = ( dat_X[:,1] - dat_X[:,1].mean() ) / dat_X[:,1].std()
-
     #====================================================
-    #   Learning Process & 汎化性能の評価
+    #   Learning Process
     #====================================================
     # classifier : random forest \n ( purity = "gini")
     forest = RandomForestClassifier(
                 criterion = "gini",     # 不純度関数 [purity]
                 bootstrap = True,       # 決定木の構築に、ブートストラップサンプルを使用するか否か（default:True）
-                #n_estimators = 2,       # 弱識別器（決定木）の数
+                n_estimators = 50,      # 弱識別器（決定木）の数
                 n_jobs = -1,            # The number of jobs to run in parallel for both fit and predict ( -1 : 全てのCPUコアで並列計算)
                 random_state = 1,       #
                 oob_score = True        # Whether to use out-of-bag samples to estimate the generalization accuracy.(default=False)
             )
-    #forest.fit( X_train_std, y_train )
+    forest.fit( X_train_std, y_train )
 
-    max_numForests = 50
-    lst_numForests = range( 1, max_numForests )     # 森のサイズのリスト（1 ~ max_numForests）
-    
-    obb_errors = []            # OOB error rate のリスト（森のサイズに対するリスト）
-    iris_errors = []           # テストに使用した Iris data 全体での誤り率のリスト （森のサイズに対するリスト）
-    setosa_errors = []         # 品種 setosa の誤り率のリスト（森のサイズに対するリスト）
-    virginica_errors = []      # 品種 virginica の誤り率のリスト（森のサイズに対するリスト）
-    versicolor_errors = []     # 品種 versicolor の誤り率のリスト（森のサイズに対するリスト）
+    #====================================================
+    #   汎化性能の評価
+    #====================================================
+    # 特徴の重要度の取得
+    feature_labels = [ 
+                       "sepal length (cm)",
+                       "sepal width (cm)",
+                       "petal length (cm)",
+                       "petal width (cm)"
+                     ]
 
-    for i in lst_numForests:
-        # 森のサイズ（弱識別器数[決定木の数]）を設定後、学習データで学習させる。
-        forest.set_params( n_estimators = i )
-        forest.fit( X_train_std, y_train )
+    importances = forest.feature_importances_
 
-        # （この森のサイズのランダムフォレストでの）OOB誤り率の計算
-        obb_error = 1 - forest.oob_score_
-        obb_errors.append( obb_error )
-        
-        # （この森のサイズのランダムフォレストでの）各特徴量の計算
-        y_predict            = forest.predict( X_test_std )
-        #y_predict_setosa     = forest.predict( dat_X_std[0:50] )
-        #y_predict_virginica  = forest.predict( dat_X_std[51:100] )
-        #y_predict_versicolor = forest.predict( dat_X_std[101:150] )
+    print( importances )
+    print( feature_labels )
 
-        error_rate           = 1 - accuracy_score( y_test, y_predict )
-        #setosa_error         = 1 - accuracy_score( dat_y, y_predict_setosa )
-        #virginica_error      = 1 - accuracy_score( dat_y, y_predict_virginica )
-        #versicolor_error     = 1 - accuracy_score( dat_y, y_predict_versicolor )
-
-        iris_errors.append( error_rate )
-        #setosa_errors.append( setosa_error )
-        #virginica_errors.append( virginica_error )
-        #versicolor_errors.append( versicolor_error )
-        
-        print( "[error rate]")
-        print( "the number of forests = ", i )
-        print( "obb_error = ", obb_error )
-        print( "error_rate = ", error_rate )
+    for f in range( X_train.shape[1] ):
+        print(
+            "%2d : %-*s %f" % (f + 1, 30, 
+            feature_labels[f], 
+            importances[f] )
+        )
 
     #------------------------------------------------------------------------
-    # 各特徴の誤り率＆OOB 誤り率 [ out-of-bag error rate] の図示
+    # 各特徴の重要度の図示
     #------------------------------------------------------------------------    
     # 現在の図をクリア
     plt.clf()
-    plt.grid()
+    #plt.grid(linestyle='-')
 
-    # OBB error rate plot(黒の●)
-    plt.plot(
-        lst_numForests, obb_errors,
-        color = "black",
-        marker = "o",
-        label = "OOB error rate",
-        markersize = 6,                 # プロット点サイズ
-        linestyle = "solid"             # 実線
+    # 各特徴の棒グラフ
+    plt.bar(
+        range( X_train.shape[1] ),  # x : トレーニング用データを 
+        importances,                # y : 
+        #color = 'lightblue', 
+        align = 'center'
     )
 
-    # iris error rate plot
-    plt.plot(
-        lst_numForests, iris_errors,
-        color = "cyan",
-        marker = "v",
-        label = "iris error rate",
-        markersize = 6,                 # プロット点サイズ
-        linestyle = "solid"             # 実線
+    # x軸の目盛り
+    plt.xticks(
+        range( X_train.shape[1] ), 
+           feature_labels, 
+           rotation = 90
     )
-    
-    """
-    # 品種 setosa の誤り率の plot(赤の□)
-    plt.plot(
-        lst_numForests, setosa_errors,
-        color = "red",
-        marker = "s",
-        label = "setosa error rate",
-        markersize = 6,                 # プロット点サイズ
-        linestyle = "solid"             # 実線
-    )
-    # 品種 virginica の誤り率の plot(青のx)
-    plt.plot(
-        lst_numForests, virginica_errors,
-        color = "blue",
-        marker = "x",
-        label = "virginica error rate",
-        markersize = 6,                 # プロット点サイズ
-        linestyle = "solid"             # 実線
-    )
-    # 品種 versicolor の誤り率の plot(緑の+)
-    plt.plot(
-        lst_numForests, versicolor_errors,
-        color = "green",
-        marker = "+",
-        label = "versicolor error rate",
-        markersize = 6,                 # プロット点サイズ
-        linestyle = "solid"             # 実線
-    )
-    """
+    # y軸の目盛り
+    plt.axhline( y = 0.1, linewidth = 0.5, color = 'k', linestyle = '--' )
+    plt.axhline( y = 0.2, linewidth = 0.5, color = 'k', linestyle = '--' )
+    plt.axhline( y = 0.3, linewidth = 0.5, color = 'k', linestyle = '--' )
+    plt.axhline( y = 0.4, linewidth = 0.5, color = 'k', linestyle = '--' )
+    plt.axhline( y = 0.5, linewidth = 0.5, color = 'k', linestyle = '--' )
 
-    plt.title("Error rate of Iris data (classifier : RandomForest)")  # titile
-    plt.xlabel("The number of trees in the forest") # label x-axis
-    plt.ylabel("Error rate") # label y-axis
+    plt.xlim( [-1, X_train.shape[1]] )
+    plt.title("Feature Importances \n classifier : RandomForest ( n_estimators = 50 )")  # titile
     plt.legend(loc = "upper left")          # 凡例
     plt.tight_layout()                      # グラフ同士のラベルが重ならない程度にグラフを小さくする。
 
     # 図の保存＆表示
-    plt.savefig("./RamdomForest_scikit-learn_3.png", dpi=300)
+    plt.savefig( "./RamdomForest_scikit-learn_4.png", dpi = 300, bbox_inches = 'tight' )
     plt.show()
     
 
