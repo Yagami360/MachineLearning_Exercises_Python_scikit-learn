dd93162e7fd4f59542553efe63f8300b3a8c6645
Yagami <y034112@gmail.com>
2017-09-03 14:19:52 +0900

    Add files via upload
    ・ main.py: 特徴行列　`X_features` 教師データ（クラスラベル）`y_labels` の命名規則に統一化
    ・ MLPolt.py : 旧クラス Plot2D に入れ替え。※ main 関数の処理は変わらない。

--- AdaLineGD/main.py
+++ AdaLineGD/main.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+# Anaconda 4.3.0 環境
+
 import numpy
 import matplotlib.pyplot as plt
 import pandas
-import Plot2D
+
+# 自作クラス
+from MLPlot import MLPlot       # 機械学習用の図の描写をサポートする関数群からなるクラス
 import AdaLineGD
 
 def main():
@@ -10,55 +14,63 @@ def main():
     #   read & set iris data
     #----------------------------------------------------
     # pandasライブラリを使用して Iris データを読み込み (dataframe obj)
-    print("reading iris data in pandas")
+    print("reading iris data from pandas-lib")
     df_Iris = pandas.read_csv('https://archive.ics.uci.edu/ml/'
         'machine-learning-databases/iris/iris.data', 
         header = None)
-    print("finish reading iris data in pandas")
+    print("finish reading iris data from pandas-lib")
     #df_Iris.tail()
 
-    dat_y = df_Iris.iloc[0:100,4].values              #
-    dat_y = numpy.where(dat_y == "Iris-setosa", -1, 1)  # Iris-setosa = -1, Iris-virginica = 1 に変換
-    dat_X = df_Iris.iloc[0:100, [0,2]].values         # pandas DataFrame のrow,columnの指定方法（iloc:rawのindex(0 ~ ),
-                                                      # columnのindex(0 ~ )）
+    y_labels = df_Iris.iloc[0:100,4].values                     #
+    y_labels = numpy.where( y_labels == "Iris-setosa", -1, 1 )  # Iris-setosa = -1, Iris-virginica = 1 に変換
+    X_features = df_Iris.iloc[0:100, [0,2]].values              # pandas DataFrame のrow,columnの指定方法（iloc:rawのindex(0 ~ ),
+                                                                # columnのindex(0 ~ )）
 
     #----------------------------------------------------
     #   Draw learning data 1 (UnNormalized)
     #----------------------------------------------------
     plt.subplot(2,2,1) # plt.subplot(行数, 列数, 何番目のプロットか)
     plt.grid(linestyle='-')
-    plt.tight_layout()  # グラフ同士のラベルが重ならない程度にグラフを小さくする。
 
     # 品種 setosa のplot(赤の○)
-    plt.scatter(dat_X[0:50,0], dat_X[0:50,1],
+    plt.scatter(
+        X_features[0:50,0], X_features[0:50,1],
         color = "red",
         marker = "o",
-        label = "setosa")
+        label = "setosa"
+    )
+
     # 品種 virginica のplot(青のx)
-    plt.scatter(dat_X[50:100,0], dat_X[50:100,1],
+    plt.scatter(
+        X_features[50:100,0], X_features[50:100,1],
         color = "blue",
         marker = "x",
-        label = "virginica")
+        label = "virginica"
+    )
 
     plt.title("Learning data [UnNormalized]")        #
     plt.xlabel("sepal length [cm]")
     plt.ylabel("petal length [cm]")   # label
     plt.legend(loc = "upper left")    # 凡例
-
+    plt.tight_layout()  # グラフ同士のラベルが重ならない程度にグラフを小さくする。
+    
     #----------------------------------------------------
     #   set AdaLineGD & draw 1
     #----------------------------------------------------
-    ada1 = AdaLineGD.AdaLineGD(lRate = 0.01, numIter=30)
-    ada1.fit(X_train = dat_X, y_train = dat_y)
+    ada1 = AdaLineGD.AdaLineGD( lRate = 0.01, numIter = 30 )
+    ada1.fit( X_train = X_features, y_train = y_labels )
     
-    ada2 = AdaLineGD.AdaLineGD(lRate = 0.0001, numIter=30)
-    ada2.fit(X_train = dat_X, y_train = dat_y)
+    ada2 = AdaLineGD.AdaLineGD( lRate = 0.0001, numIter = 30 )
+    ada2.fit( X_train = X_features, y_train = y_labels )
 
     plt.subplot(2,2,2) # plt.subplot(行数, 列数, 何番目のプロットか)
     plt.grid(linestyle='-')
-    plt.plot(range(1, len(ada1.cost_) + 1), 
-        numpy.log10(ada1.cost_), 
-        marker = 'o')
+    
+    plt.plot(
+        range(1, len(ada1.cost_) + 1), numpy.log10(ada1.cost_), 
+        marker = 'o'
+    )
+
     plt.xlabel("Epochs")
     plt.ylabel("log(Sum-squared-error)")
     plt.title("Adaline - Learning rate 0.01")
@@ -66,9 +78,12 @@ def main():
 
     plt.subplot(2,2,3) # plt.subplot(行数, 列数, 何番目のプロットか)
     plt.grid(linestyle='-')
-    plt.plot(range(1, len(ada2.cost_) + 1), 
-        ada2.cost_, 
-        marker = 'o')
+
+    plt.plot(
+        range(1, len(ada2.cost_) + 1), ada2.cost_, 
+        marker = 'o'
+    )
+
     plt.xlabel("Epochs")
     plt.ylabel("Sum-squared-error")
     plt.title("Adaline - Learning rate 0.0001")
@@ -79,7 +94,7 @@ def main():
     plt.ylabel("petal length [Normalized]")        # label
     plt.title("Adaline - Learning rate 0.0001")
     plt.tight_layout()  # グラフ同士のラベルが重ならない程度にグラフを小さくする。
-    Plot2D.Plot2D.drawDiscriminantRegions( dat_X, dat_y, ada2 )
+    MLPlot.drawDiscriminantRegions( X_features, y_labels, ada2 )
     
     plt.savefig("./AdalineGD_1.png", dpi=300)
     plt.show()
@@ -88,23 +103,28 @@ def main():
     #----------------------------------------------------
     #   normalized lerning data & draw
     #----------------------------------------------------
-    dat_X_std = numpy.copy(dat_X)                                           # ディープコピー（参照コピーではない）
-    dat_X_std[:,0] = ( dat_X[:,0] - dat_X[:,0].mean() ) / dat_X[:,0].std()  # 0列目全てにアクセス[:,0]
-    dat_X_std[:,1] = ( dat_X[:,1] - dat_X[:,1].mean() ) / dat_X[:,1].std()
+    X_features_std = numpy.copy( X_features )                                                          # ディープコピー（参照コピーではない）
+    X_features_std[:,0] = ( X_features[:,0] - X_features[:,0].mean() ) / X_features[:,0].std()  # 0列目全てにアクセス[:,0]
+    X_features_std[:,1] = ( X_features[:,1] - X_features[:,1].mean() ) / X_features[:,1].std()
 
     plt.subplot(2,2,1) # plt.subplot(行数, 列数, 何番目のプロットか)
-    plt.grid(linestyle='-')
+    plt.grid( linestyle='-' )
 
     # 品種 setosa のplot(赤の○)
-    plt.scatter(dat_X_std[0:50,0], dat_X_std[0:50,1],
+    plt.scatter(
+        X_features_std[0:50,0], X_features_std[0:50,1],
         color = "red",
         marker = "o",
-        label = "setosa")
+        label = "setosa"
+    )
+
     # 品種 virginica のplot(青のx)
-    plt.scatter(dat_X_std[50:100,0], dat_X_std[50:100,1],
+    plt.scatter(
+        X_features_std[50:100,0], X_features_std[50:100,1],
         color = "blue",
         marker = "x",
-        label = "virginica")
+        label = "virginica"
+    )
 
     plt.title("Learning data [Normalized]")        #
     plt.xlabel("sepal length [Normalized]")
@@ -115,14 +135,17 @@ def main():
     #----------------------------------------------------
     #   set AdaLineGD & draw 2 (Normalized)
     #----------------------------------------------------
-    ada3 = AdaLineGD.AdaLineGD(lRate = 0.01, numIter=30)
-    ada3.fit(dat_X_std, dat_y)
+    ada3 = AdaLineGD.AdaLineGD( lRate = 0.01, numIter = 30 )
+    ada3.fit( X_features_std, y_labels )
    
     plt.subplot(2,2,2) # plt.subplot(行数, 列数, 何番目のプロットか)
     plt.grid(linestyle='-')
-    plt.plot(range(1, len(ada3.cost_) + 1), 
-        ada3.cost_, 
-        marker = 'o')
+
+    plt.plot(
+        range(1, len(ada3.cost_) + 1), ada3.cost_, 
+        marker = 'o'
+    )
+
     plt.xlabel("Epochs")
     plt.ylabel("Sum-squared-error")
     plt.title("Adaline - Learning rate 0.01")
@@ -134,7 +157,7 @@ def main():
     plt.title("Adaline - Learning rate 0.01")
     plt.tight_layout()  # グラフ同士のラベルが重ならない程度にグラフを小さくする。
 
-    Plot2D.Plot2D.drawDiscriminantRegions( dat_X_std, dat_y, ada3 )
+    MLPlot.drawDiscriminantRegions( X_features_std, y_labels, ada3 )
 
     plt.savefig("./AdalineGD_2.png", dpi=300)
     plt.show()
