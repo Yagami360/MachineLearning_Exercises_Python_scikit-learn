cdd9f904974f8d572b73672d0be1dfc4f6ee7608
Yagami <y034112@gmail.com>
2017-09-03 15:22:19 +0900

    Add files via upload
    ・ main.py: 特徴行列　`X_features` 教師データ（クラスラベル）`y_labels` の命名規則に統一化
    ・ MLPolt.py : 旧クラス Plot2D に入れ替え。※ main 関数の処理は変わらない。

--- LogisticRegression_scikit-learn/main.py
+++ LogisticRegression_scikit-learn/main.py
@@ -12,7 +12,7 @@ from sklearn.preprocessing import StandardScaler        # scikit-learn の prepr
 from sklearn.metrics import accuracy_score              # 
 
 # 自作クラス
-import Plot2D
+from MLPlot import MLPlot                               # 機械学習用の図の描写をサポートする関数群からなるクラス
 import LogisticRegression
 
 def main():
@@ -31,11 +31,11 @@ def main():
     print("reading data")
     # scikit-learn ライブラリから iris データを読み込む
     iris = datasets.load_iris()
-    # 3,4 列目の特徴量を抽出し、dat_X に保管
-    dat_X = iris.data[ :, [2,3] ]
+    # 3,4 列目の特徴量を抽出し、X_features に保管
+    X_features = iris.data[ :, [2,3] ]
     # クラスラベル（教師データ）を取得
-    dat_y = iris.target
-    print( 'Class labels:', numpy.unique(dat_y) )    # ※多くの機械学習ライブラリクラスラベルは文字列から整数にして管理されている（最適な性能を発揮するため）
+    y_labels = iris.target
+    print( 'Class labels:', numpy.unique(y_labels) )    # ※多くの機械学習ライブラリクラスラベルは文字列から整数にして管理されている（最適な性能を発揮するため）
     print("finishing reading data")
 
     #---------------------------------------------------------------------
@@ -43,10 +43,10 @@ def main():
     # データセットをトレーニングデータセットとテストデータセットに分割する
     #---------------------------------------------------------------------
     # scikit-learn の cross_validation モジュールの関数 train_test_split() を用いて、70%:テストデータ, 30%:トレーニングデータに分割
-    train_test = train_test_split(       # 戻り値:list
-                     dat_X, dat_y,       # 
-                     test_size = 0.3,    # 0.0~1.0 で指定 
-                     random_state = 0    # 
+    train_test = train_test_split(               # 戻り値:list
+                     X_features, y_labels,       # 
+                     test_size = 0.3,            # 0.0~1.0 で指定 
+                     random_state = 0            # 
                  )
     """
     # train_test_split() の戻り値の確認
@@ -79,9 +79,9 @@ def main():
     y_combined     = numpy.hstack( (y_train, y_test) )
 
     # 学習データを正規化（後で plot データ等で使用する）
-    #dat_X_std = numpy.copy(dat_X)                                           # ディープコピー（参照コピーではない）
-    #dat_X_std[:,0] = ( dat_X[:,0] - dat_X[:,0].mean() ) / dat_X[:,0].std()  # 0列目全てにアクセス[:,0]
-    #dat_X_std[:,1] = ( dat_X[:,1] - dat_X[:,1].mean() ) / dat_X[:,1].std()
+    #X_features_std = numpy.copy( X_features )                                   # ディープコピー（参照コピーではない）
+    #X_features_std[:,0] = ( X_features[:,0] - X_features[:,0].mean() ) / X_features[:,0].std()  # 0列目全てにアクセス[:,0]
+    #X_features_std[:,1] = ( X_features[:,1] - X_features[:,1].mean() ) / X_features[:,1].std()
 
     #====================================================
     #   Learning Process (& 説明用の図のpolt)
@@ -106,8 +106,8 @@ def main():
     #-------------------------------
     # 識別結果＆識別領域の表示
     #-------------------------------
-    Plot2D.Plot2D.drawDiscriminantRegions( 
-        dat_X = X_combined_std, dat_y = y_combined,
+    MLPlot.drawDiscriminantRegions( 
+        X_combined_std, y_combined,
         classifier = logReg.logReg_ ,
         list_test_idx = range( 101,150 )
     )
@@ -216,7 +216,7 @@ def main():
     # 10個の逆正則化パラメータ C（C=10^-5,C=10^-4, ... C=10, ... , C=10^5 ）に関して、LogisiticReegression オブジェクトを作成し、
     # それぞれのモデルを学習データで学習
     for c in numpy.arange(-5, 5):
-        logReg_tmp = LogisticRegression.LogisticRegression( paramC = 10**c )
+        logReg_tmp = LogisticRegression.LogisticRegression( paramC = 10.**c )
         logReg_tmp.logReg_.fit( X_train_std, y_train )
 
         # 重み係数リストに学習後の重み係数を格納
@@ -229,7 +229,7 @@ def main():
         #print("weight2:", logReg_tmp.logReg_.coef_[2] )
 
         # 逆正則化パラメータリストにモデルの C 値を格納
-        paramesC.append( 10**c )
+        paramesC.append( 10.**c )
 
     # 重み係数リストを numpy 配列に変換
     weights0 = numpy.array( weights0 )
